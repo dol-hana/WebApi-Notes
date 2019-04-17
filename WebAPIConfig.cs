47759975e7d42f34fd03620f248ada714018f01c
dol-hana <dol_hana@hotmail.com>
2019-04-17 10:19:17 -0400

    third

--- WebAPIConfig.cs
+++ WebAPIConfig.cs
@@ -22,8 +22,10 @@ public static class WebApiConfig
         }
     }
 
-
-
+// Routing 
+routing : api/{controller}/{id}
+	http://localhost:23421/api/values/5
+	
 namespace API.Controllers
 {
     [Authorize]
@@ -57,3 +59,162 @@ namespace API.Controllers
         }
     }
 }
+
+// =============================================================================================
+//Sample : products
+/* 1. Build model
+   2. Build the repository
+   3. Build the controller
+*/
+
+//1 .Build Model 
+public class product
+{
+	public string Description {get;set;}
+	public decimal Price {get;set;}
+	public string ProductCode {get;set;}
+	public int ProductId {get;set;}
+	public string ProductName {get;set;}
+	public DateTime ReleaseDate {get;set;}
+
+}
+ 
+//2. Build the repository
+// Controller <--> Repository <--> data Source
+                        ^
+                        |
+                      Model 
+
+//
+// @"~/App_Data/product.json" file 
+//
+[
+  
+  {
+    
+   "Description": "Leaf rake with 48-inch wooden handle.",
+   
+   "Price": 19.95,
+    
+   "ProductCode": "GDN-0011",
+    
+   "ProductId": 1,
+    
+   "ProductName": "Leaf Rake",
+    
+   "ReleaseDate": "2009-03-19T00:00:00-07:00"
+  
+},
+  
+{
+    
+   "Description": "15 gallon capacity rolling garden cart",
+    
+   "Price": 32.99,
+    
+   "ProductCode": "GDN-0023",
+    
+   "ProductId": 2,
+    
+   "ProductName": "Garden Cart",
+    
+   "ReleaseDate": "2010-03-18T00:00:00-07:00"
+  }
+]
+
+// ProductRepository.cs
+namespace APM.WebAPI.Models
+{
+    /// <summary>
+    /// Stores the data in a json file so that no database is required for this
+    /// sample application
+    /// </summary>
+    public class ProductRepository
+    {
+        /// <summary>
+        /// Creates a new product with default values
+        /// </summary>
+        /// <returns></returns>
+        internal Product Create()
+        {
+            Product product = new Product
+            {
+                ReleaseDate = DateTime.Now
+            };
+            return product;
+        }
+
+        /// <summary>
+        /// Retrieves the list of products.
+        /// </summary>
+        /// <returns></returns>
+        internal List<Product> Retrieve()
+        {
+            var filePath = HostingEnvironment.MapPath(@"~/App_Data/product.json");
+
+            var json = System.IO.File.ReadAllText(filePath);
+
+            var products = JsonConvert.DeserializeObject<List<Product>>(json);
+
+            return products;
+        }
+
+        /// <summary>
+        /// Saves a new product.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        internal Product Save(Product product)
+        {
+            // Read in the existing products
+            var products = this.Retrieve();
+
+            // Assign a new Id
+            var maxId = products.Max(p => p.ProductId);
+            product.ProductId = maxId + 1;
+            products.Add(product);
+
+            WriteData(products);
+            return product;
+        }
+
+        /// <summary>
+        /// Updates an existing product
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        internal Product Save(int id, Product product)
+        {
+            // Read in the existing products
+            var products = this.Retrieve();
+
+            // Locate and replace the item
+            var itemIndex = products.FindIndex(p => p.ProductId == product.ProductId);
+            if (itemIndex > 0)
+            {
+                products[itemIndex] = product;
+            }
+            else
+            {
+                return null;
+            }
+
+            WriteData(products);
+            return product;
+        }
+
+        private bool WriteData(List<Product> products)
+        {
+            // Write out the Json
+            var filePath = HostingEnvironment.MapPath(@"~/App_Data/product.json");
+
+            var json = JsonConvert.SerializeObject(products, Formatting.Indented);
+            System.IO.File.WriteAllText(filePath, json);
+
+            return true;
+        }
+
+    }
+}
+
